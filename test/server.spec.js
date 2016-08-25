4c41fc565103c61558d291ca1b69d5a7f6a1470e
Jayro Salgado <jsalgado@nearsoft.com>
2016-08-24 20:11:23 -0700

    test refactor

--- test/server.spec.js
+++ test/server.spec.js
@@ -4,7 +4,7 @@ var app = require('../index.js');
 describe('GET /user', function() {
   it('respond with json', function(done) {
     request(app)
-      .get('/user')
+      .get('/')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200, done);
