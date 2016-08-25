d64d3ab73bead7d6fb6889a5fdabe34bdbf69343
Jayro Salgado <jsalgado@nearsoft.com>
2016-08-25 10:05:40 -0700

    again

--- test/index.js
+++ test/index.js
@@ -1,12 +1,11 @@
 var request = require('supertest'); 
 var app = require('../index.js');
 
-describe('GET /user', function() {
+describe('GET /', function() {
   it('respond with json', function(done) {
     request(app)
       .get('/')
       .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
       .expect(200, done);
   });
 });
