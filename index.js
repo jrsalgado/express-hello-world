f53c099e4fe0a7f529acf94e3391e297daa1bb75
Jayro Salgado <jsalgado@NSL-JSALGADOs-MacBook-Pro.local>
2018-02-17 11:25:29 -0700

    last changes

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
 var express = require('express');
 var app = express();
 var http = require('http');
-var port = 8080;
 var bodyParser = require('body-parser');
 var path = require('path');
+var validator = require('express-validation');
+var joi = require('joi');
+var fetch = require('isomorphic-fetch');
+
+const GAPI ={
+  "user_url": "https://api.github.com/users/",
+}
+
+var port = 8080;
+
+app.use(bodyParser.json())
 
 app.set('views', path.join(__dirname, './web'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, './web')));
 app.get('/', (req, res) => res.render("index.html"));
 
-app.get('/api', function (req, res) {
-  res.json({
-    msg: 'Hello from New Build',
-    todo: ['Connecto webhook to circleCi','error messaging service','Migrate all'],
-    test: 'test'
-  });
+var getuserSchema = {
+  params: {
+    user: joi.string().required()
+  }
+}
+
+app.get('/api/user/:user',
+  validator(getuserSchema),
+  async function (req, res) {
+
+    const user = await fetchPeople(req.params.user);
+
+    res.json({user});
 });
 
 var httpServer = http.createServer(app);
 httpServer.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
 });
+
+async function fetchPeople(user){
+  let response;
+  try {
+    response= await fetch(`${GAPI.user_url}${user}`);
+  } catch (error) {
+    console.error(error);
+  }
+  return await response.json();  
+}
